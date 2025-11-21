2d6cc7cac0353daa4f00e26577d98f5036b4079c
Hristo Davidov <87039429+HristoDavidov@users.noreply.github.com>
2025-11-21 18:11:49 +0100

    Install and setup react-router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <App />
+  <BrowserRouter>
+  
+    <App />
+  
+  </BrowserRouter>
+  
 
-)
+);
