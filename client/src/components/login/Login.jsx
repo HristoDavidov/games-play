7c72b658a3b921f498a4dd27ce6dd61263dba1c5
Hristo Davidov <87039429+HristoDavidov@users.noreply.github.com>
2026-01-28 15:56:01 +0100

    Implement login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,17 +1,50 @@
+import { useLogin } from "../../hooks/useAuth";
+import { useForm } from "../../hooks/useForm";
+
+import { useNavigate} from "react-router-dom";
+
 export default function Login() {
+
+    const login = useLogin();
+    const navigate = useNavigate();
+
+    const {values, changeHandler, submitHandler} = useForm({ email: '', password: '' }, ({ email, password }) => {
+
+        try {
+            login(email, password);
+            navigate('/');
+        } catch (err) {
+            console.log(err.message);
+            
+        }
+        
+    });
 
     return (
 
         <section id="login-page" className="auth">
-            <form id="login">
-                    <div className="container">
+            <form id="login" onSubmit={submitHandler}>
+                <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email"
+                        value={values.email}
+                        onChange={changeHandler}
+                        placeholder="Sokka@gmail.com" 
+                    />
 
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input 
+                        type="password" 
+                        id="login-password" 
+                        name="password"
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <a href="#">here</a></span>
